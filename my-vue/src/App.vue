f03f4356b0a1ecb43d60d60a5fad4e7c338cb2f6
NiuSailong <Niusailong@126.com>
2020-03-17 00:25:26 +0800

    updata

--- my-vue/src/App.vue
+++ my-vue/src/App.vue
@@ -1,16 +1,15 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="less">
-  #app{
-    width: 100%;
-    height: 100%;
-  }
+#app {
+  width: 100%;
+  height: 100%;
+  min-width: 960px;
+  overflow: hidden;
+  overflow-y: auto;
+}
 </style>
